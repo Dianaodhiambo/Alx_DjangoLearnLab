0334ba1797d794373a96e07e2e018f5c935d9c9e
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2024-09-21 22:56:36 +0300

    Add templates for list_books view

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,21 +1,24 @@
 # relationship_app/views.py
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book, Library  # Ensure you import Library
 
-# This view should render a simple text list of book titles and their authors.
+# Function-based view for listing books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'list_books.html', {'books': books})
 
-# Create a class-based view in relationship_app/views.py that displays details for a specific library,
-# listing all books available in that library.
+# Class-based view for library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
+    template_name = 'library_detail.html'  # Ensure you create this template
+    context_object_name = 'library'  # This will be used in the template
 
-    # Utilize Django’s ListView or DetailView to structure this class-based view.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()  # Fetch books related to the library
+        return context
+
 
 
 
